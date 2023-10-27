808608dbd39c683322bc3ecef74ca9a7d62347fb
Melis Ölmez <olmezmelis204@gmail.com>
2023-10-27 14:31:45 +0300

    added user entity class

---
+++ questapp/questapp/src/main/java/com/project/questapp/entity/User.java
@@ -0,0 +1,31 @@
+package com.project.questapp.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String username;
+    private String password;
+
+    public Long getId(){
+        return id;
+    }
+    public void setPassword(String password){
+        this.password=password;
+    }
+    public String getPassword(){
+        return password;
+    }
+    public void setUsername(String username){
+        this.username=username;
+    }
+    public String getUsername(){
+        return username;
+    }
+}
