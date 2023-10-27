8decb863ae227bbd1e9e53334e3858036c59fd84
Melis Ölmez <olmezmelis204@gmail.com>
2023-10-27 16:58:02 +0300

    created a interface for user service

---
+++ questapp/questapp/src/main/java/com/project/questapp/service/user/UserServiceInterface.java
@@ -0,0 +1,21 @@
+package com.project.questapp.service.user;
+
+import com.project.questapp.entity.User;
+import org.springframework.validation.annotation.Validated;
+
+import java.util.List;
+import java.util.Optional;
+
+@Validated
+public interface UserServiceInterface {
+    List<User> findAllUsers();
+
+    User saveUser(User user);
+
+    Optional<User> findUserById(Long id);
+
+    User updateUser(User user,Long id);
+
+    void deleteUserById(Long id);
+
+}
