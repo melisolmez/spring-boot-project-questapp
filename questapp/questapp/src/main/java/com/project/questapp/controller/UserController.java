8bfaf5ca8ae6d3db0f9ebee52fd1a3afbda941be
Melis Ölmez <olmezmelis204@gmail.com>
2023-10-28 19:46:04 +0300

    update user controller

--- questapp/questapp/src/main/java/com/project/questapp/controller/UserController.java
+++ questapp/questapp/src/main/java/com/project/questapp/controller/UserController.java
@@ -2,7 +2,8 @@ package com.project.questapp.controller;
 
 import com.project.questapp.entity.User;
 import com.project.questapp.service.user.UserService;
-import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -23,7 +24,29 @@ public class UserController {
 
     @PostMapping
     public User save(@RequestBody UserServiceRequest user){
-      return userService.saveUser(user.toServiceRequest());
+        return userService.saveUser(user.toServiceRequest());
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<?> findUserById(@PathVariable Long id){
+        if(userService.findUserById(id).isPresent()){
+            return new ResponseEntity<>("User found",HttpStatus.OK);
+        }
+        return new ResponseEntity<>("User not found",HttpStatus.NOT_FOUND);
+    }
+    @PutMapping("/{id}")
+    public ResponseEntity<?> updateUser(@RequestBody UserServiceRequest user,@PathVariable Long id){
+        if(userService.updateUser(user.toServiceRequest(),id)!=null){
+            return new ResponseEntity<>("User updated successfully",HttpStatus.OK);
+        }
+       return new ResponseEntity<>("User not found",HttpStatus.NOT_FOUND);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteUserById(@PathVariable Long id){
+        if(userService.deleteUserById(id)){
+            return new ResponseEntity<>("User deleted successfully", HttpStatus.NO_CONTENT);
+        }
+        return new ResponseEntity<>("User not found",HttpStatus.NOT_FOUND);
     }
 
 }
