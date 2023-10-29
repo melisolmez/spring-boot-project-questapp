48fe21dff0f962dd05e7f9d78bc6ec3996811893
Melis Ölmez <olmezmelis204@gmail.com>
2023-10-29 20:58:26 +0300

    added comment service

---
+++ questapp/questapp/src/main/java/com/project/questapp/service/comment/CommentService.java
@@ -0,0 +1,35 @@
+package com.project.questapp.service.comment;
+
+import com.project.questapp.entity.User;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CommentService implements CommentServiceInterface {
+    @Override
+    public List<User> findAllUsers() {
+        return null;
+    }
+
+    @Override
+    public User saveUser(User user) {
+        return null;
+    }
+
+    @Override
+    public Optional<User> findUserById(Long id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public User updateUser(User user, Long id) {
+        return null;
+    }
+
+    @Override
+    public boolean deleteUserById(Long id) {
+        return false;
+    }
+}
