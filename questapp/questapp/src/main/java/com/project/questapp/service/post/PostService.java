0281594210009ede57a8d76523cf0f8c7f979b66
Melis Ölmez <olmezmelis204@gmail.com>
2023-10-31 16:57:47 +0300

    update post service

--- questapp/questapp/src/main/java/com/project/questapp/service/post/PostService.java
+++ questapp/questapp/src/main/java/com/project/questapp/service/post/PostService.java
@@ -2,19 +2,55 @@ package com.project.questapp.service.post;
 
 import com.project.questapp.entity.Post;
 import com.project.questapp.entity.User;
+import com.project.questapp.repository.PostRepository;
+import com.project.questapp.request.PostRequest;
+import com.project.questapp.service.user.UserService;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class PostService implements PostServiceInterface {
-    @Override
-    public List<Post> getAllPosts() {
-        return null;
+
+    private final PostRepository postRepository;
+
+    private final UserService userService;
+    public PostService(PostRepository postRepository, UserService userService) {
+        this.postRepository = postRepository;
+        this.userService = userService;
     }
 
     @Override
-    public List<Post> getPostById(User userId) {
-        return null;
+    public List<Post> getAllPosts(Optional<Long> userId) {
+        if(userId.isPresent()){
+            return postRepository.findByUserId(userId);
+        }
+        return postRepository.findAll();
     }
+
+    @Override
+    public Optional<Post> getOnePostById(Long postId) {
+        Post existPosting=postRepository.findById(postId).orElse(null);
+        if(existPosting==null){
+            return null;
+        }
+        return postRepository.findById(postId);
+    }
+
+    @Override
+    public Post savePost(Post post) {
+      Optional<User> userFromDb=userService.findUserById(post.getUser().getId());
+      if(userFromDb.isEmpty()){
+          return null;
+      }
+      postRepository.save(post);
+      return post;
+    }
+
+    @Override
+    public void deletePostById(Long id) {
+        postRepository.deleteById(id);
+    }
+
 }
