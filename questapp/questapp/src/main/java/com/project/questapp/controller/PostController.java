08f894876f79bcaa7337fff91093e926037a7206
Melis Ölmez <olmezmelis204@gmail.com>
2023-10-31 16:58:15 +0300

    update post post controller

--- questapp/questapp/src/main/java/com/project/questapp/controller/PostController.java
+++ questapp/questapp/src/main/java/com/project/questapp/controller/PostController.java
@@ -1,8 +1,38 @@
 package com.project.questapp.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.project.questapp.entity.Post;
+import com.project.questapp.request.PostRequest;
+import com.project.questapp.service.post.PostService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
+@RequestMapping("/posts")
 public class PostController {
+
+    private final PostService postService;
+    public PostController(PostService postService) {
+        this.postService = postService;
+    }
+    @GetMapping
+    public List<Post> getAllPosts(@RequestParam Optional<Long> userId){
+       return postService.getAllPosts(userId);
+    }
+    @GetMapping("/{postid}")
+    public Optional<Post> getOnePostById(@PathVariable long postid){
+        return postService.getOnePostById(postid);
+    }
+
+    @PostMapping
+    public Post createOnePost(@RequestBody PostRequest postRequest){
+        return postService.savePost(postRequest.toPostService());
+    }
+
+    @DeleteMapping("/{postId}")
+    public void deletePost(@PathVariable long postId){
+        postService.deletePostById(postId);
+    }
 
 }
