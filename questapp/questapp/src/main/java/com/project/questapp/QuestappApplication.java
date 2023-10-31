a7ea53cc4ad81dcd384e416d5eba0b8dc48c9855
Melis Ölmez <olmezmelis204@gmail.com>
2023-10-31 17:00:38 +0300

    update project

--- questapp/questapp/src/main/java/com/project/questapp/QuestappApplication.java
+++ questapp/questapp/src/main/java/com/project/questapp/QuestappApplication.java
@@ -8,36 +8,7 @@ public class QuestappApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(QuestappApplication.class, args);
-		/*
-		 @Override
-    public List<User> findAllUser(){
-        return userRepository.findAll();
-    }
-    public User saveUser(User user){
-        return userRepository.save(user);
-    }
-    public Optional<User> findUserById(Long id){
-       Optional<User> userOptional=userRepository.findById(id);
-       if(userOptional.isPresent())
-        return userRepository.findById(id);
-
-       return Optional.empty();
-    }
-    public User updateUser(User user, Long id){
-        User existingUser=userRepository.findById(id).orElse(null);
-        if(existingUser==null){
-           return null;
-        }
-        existingUser.setPassword(user.getPassword());
-        existingUser.setUsername(user.getUsername());
-        userRepository.save(existingUser);
-
-        return existingUser;
-    }
-    public void deleteUserById(Long id){
-        userRepository.deleteById(id);
-    }
-		 */
+		
 	}
 
 }
