765116871f0b7740fd8f5080168f64a6bbfb5ec2
Melis Ölmez <olmezmelis204@gmail.com>
2023-10-31 16:57:59 +0300

    update post service interface

--- questapp/questapp/src/main/java/com/project/questapp/service/post/PostServiceInterface.java
+++ questapp/questapp/src/main/java/com/project/questapp/service/post/PostServiceInterface.java
@@ -2,15 +2,21 @@ package com.project.questapp.service.post;
 
 
 import com.project.questapp.entity.Post;
-import com.project.questapp.entity.User;
+import com.project.questapp.request.PostRequest;
 import org.springframework.validation.annotation.Validated;
 
 import java.util.List;
+import java.util.Optional;
 
 @Validated
 public interface PostServiceInterface {
 
-    List<Post> getAllPosts();
+    List<Post> getAllPosts(Optional<Long> userId);
 
-    List<Post> getPostById(User userId);
+    Optional<Post> getOnePostById(Long postId);
+
+    Post savePost(Post post);
+
+    void deletePostById(Long id);
+
 }
