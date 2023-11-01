3d1607eeae7508e0ca81d718e565cf80843a1804
Melis Ölmez <olmezmelis204@gmail.com>
2023-11-01 22:25:43 +0300

    update post request record class

--- questapp/questapp/src/main/java/com/project/questapp/controller/PostRequest.java
+++ questapp/questapp/src/main/java/com/project/questapp/controller/PostRequest.java
@@ -1,7 +1,7 @@
-package com.project.questapp.request;
+package com.project.questapp.controller;
 
-import com.project.questapp.entity.Post;
-import com.project.questapp.entity.User;
+import com.project.questapp.model.Post;
+import com.project.questapp.model.User;
 
 public record PostRequest(
         Long userId,
