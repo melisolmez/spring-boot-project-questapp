59cf396aa182a0469ccb7d33c76da07a6f261e7e
Melis Ölmez <olmezmelis204@gmail.com>
2023-11-01 22:24:33 +0300

    update like controller class

--- questapp/questapp/src/main/java/com/project/questapp/controller/LikeController.java
+++ questapp/questapp/src/main/java/com/project/questapp/controller/LikeController.java
@@ -1,7 +1,31 @@
 package com.project.questapp.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.project.questapp.model.Like;
+import com.project.questapp.service.like.LikeService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
+@RequestMapping("/like")
 public class LikeController {
+
+    private final LikeService likeService;
+    public LikeController(LikeService likeService) {
+        this.likeService = likeService;
+    }
+
+    @GetMapping
+    public List<Like> getAllLike(@RequestParam Optional<Long> userId,
+                                 @RequestParam Optional<Long> postId){
+        return likeService.getAllLike(userId,postId);
+    }
+    @GetMapping("/{likeId}")
+    public Like getOneLike(@PathVariable Long likeId){
+        return likeService.getOneLikeById(likeId);
+    }
+
+
+
 }
