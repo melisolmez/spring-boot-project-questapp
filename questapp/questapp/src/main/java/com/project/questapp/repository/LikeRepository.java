82bc9cb47b98d9d249336d1ff79c932fabf6b310
Melis Ölmez <olmezmelis204@gmail.com>
2023-11-01 22:21:57 +0300

    added method in like repository

--- questapp/questapp/src/main/java/com/project/questapp/repository/LikeRepository.java
+++ questapp/questapp/src/main/java/com/project/questapp/repository/LikeRepository.java
@@ -1,9 +1,18 @@
 package com.project.questapp.repository;
 
-import com.project.questapp.entity.Like;
+import com.project.questapp.model.Like;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface LikeRepository  extends JpaRepository<Like,Long> {
+
+    List<Like> findByUserIdAndPostId(Optional<Long> userId, Optional<Long> postId);
+
+    List<Like> findbyUserId(Optional<Long> userId);
+
+    List<Like> findByPostId(Optional<Long> postId);
 }
