11ebe06d2a8eccf05925ebf6ec15b1e42201c2af
Melis Ölmez <olmezmelis204@gmail.com>
2023-11-01 22:21:41 +0300

    added method in comment repository

--- questapp/questapp/src/main/java/com/project/questapp/repository/CommentRepository.java
+++ questapp/questapp/src/main/java/com/project/questapp/repository/CommentRepository.java
@@ -1,9 +1,16 @@
 package com.project.questapp.repository;
 
-import com.project.questapp.entity.Comment;
+import com.project.questapp.model.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CommentRepository extends JpaRepository<Comment,Long> {
+    List<Comment> findByUserIdAndPostId(Long aLong, Long aLong1);
+
+    List<Comment> findByUserId(Long aLong);
+
+    List<Comment> findByPostId(Long aLong);
 }
