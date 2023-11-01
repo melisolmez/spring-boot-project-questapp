b16c4352267132922bf4abccd3df2a63a802f70c
Melis Ölmez <olmezmelis204@gmail.com>
2023-11-01 22:27:23 +0300

    update user repository

--- questapp/questapp/src/main/java/com/project/questapp/repository/UserRepository.java
+++ questapp/questapp/src/main/java/com/project/questapp/repository/UserRepository.java
@@ -1,6 +1,6 @@
 package com.project.questapp.repository;
 
-import com.project.questapp.entity.User;
+import com.project.questapp.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
