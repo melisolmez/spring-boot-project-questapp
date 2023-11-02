0f07e761d7c57b903f68b163f0ee27dc28896925
Melis Ölmez <olmezmelis204@gmail.com>
2023-11-02 17:28:31 +0300

    update comment controller

--- questapp/questapp/src/main/java/com/project/questapp/controller/CommentController.java
+++ questapp/questapp/src/main/java/com/project/questapp/controller/CommentController.java
@@ -2,6 +2,8 @@ package com.project.questapp.controller;
 
 import com.project.questapp.model.Comment;
 import com.project.questapp.service.comment.CommentService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -18,27 +20,46 @@ public class CommentController {
 
 
     @GetMapping
-    public List<Comment> getAllComments(@RequestParam Optional<Long> userId,
-                                        @RequestParam Optional<Long> postId){
-                return commentService.getAllCommentsWithParam(userId,postId);
+    public ResponseEntity<List<Comment>> getAllComments(@RequestParam Optional<Long> userId,
+                                                       @RequestParam Optional<Long> postId){
+       List<Comment> comments=commentService.getAllCommentsWithParam(userId,postId);
+       if(!comments.isEmpty()){
+           return new ResponseEntity<>(comments, HttpStatus.OK);
+       }
+       return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
     @GetMapping("/{commentId}")
-    public Comment getOneComment(@PathVariable Long commentId){
-        return commentService.getOneCommentById(commentId);
+    public ResponseEntity<Comment> getOneComment(@PathVariable Long commentId){
+        Comment comment=commentService.getOneCommentById(commentId);
+       if(comment!=null){
+           return new ResponseEntity<>(comment,HttpStatus.OK);
+       }
+       return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
     @PostMapping
-    public Comment createOneComment(@RequestBody CommentRequest commentRequest){
-        return commentService.createOneComment(commentRequest.toCommentRequest());
+    public ResponseEntity<Comment> createOneComment(@RequestBody CommentRequest commentRequest){
+        Comment comment= commentService.createOneComment(commentRequest.toCommentRequest());
+        if(comment!=null){
+            return new ResponseEntity<>(comment,HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
     @PutMapping("/{commentId}")
-    public Comment updateOneComment(@PathVariable Long commentId,@RequestBody CommentUpdateRequest updateRequest){
-        return commentService.updateOneCommentById(commentId,updateRequest.toComment());
+    public ResponseEntity<Comment> updateOneComment(@PathVariable Long commentId,@RequestBody CommentUpdateRequest updateRequest){
+        Comment comment=commentService.updateOneCommentById(commentId,updateRequest.toComment());
+        if(comment!=null){
+            return new ResponseEntity<>(comment,HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
      @DeleteMapping("/{commentId}")
-    public void deleteCommentById(@PathVariable Long commentId){
-         commentService.deleteCommentById(commentId);
+    public ResponseEntity<String> deleteCommentById(@PathVariable Long commentId){
+        if(commentService.deleteCommentById(commentId)) {
+            return new ResponseEntity<>("Post deleted successfully",HttpStatus.OK);
+        }
+         return new ResponseEntity<>("Post not deleted successfully",HttpStatus.NOT_FOUND);
      }
 }
